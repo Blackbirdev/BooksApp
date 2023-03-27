99c2c4e0d869b45ea3eea853fad67d938bea843f
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-03-27 19:06:04 +0200

    Add book filtering using a form

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,8 @@
     }
   };
   const classNames = {
-    favorite: 'favorite'
+    favorite: 'favorite',
+    hidden: 'hidden',
   };
 
   const templates = {
@@ -26,6 +27,7 @@
       const thisBooksList = this;
 
       thisBooksList.favoriteBooks = [];
+      thisBooksList.filters = [];
 
       thisBooksList.getElements();
       thisBooksList.render();
@@ -44,6 +46,7 @@
       const thisBooksList = this;
 
       thisBooksList.booksContainer = document.querySelector(select.containerOf.list);
+      thisBooksList.booksFilter = document.querySelector(select.containerOf.filters);
     }
     initActions() {
       const thisBooksList = this;
@@ -59,6 +62,44 @@
           thisBooksList.favoriteBooks.pop(targetBook);
         }
       });
+
+      thisBooksList.booksFilter.addEventListener('click', (event) => {
+        const filter = event.target;
+
+        if (filter.tagName == 'INPUT' && filter.type == 'checkbox' && filter.name == 'filter'
+        ) {
+          let filterValue = filter.value;
+          console.log('filterValue:', filterValue);
+
+          if (filter.checked == true) {
+            thisBooksList.filters.push(filterValue);
+          } else {
+            thisBooksList.filters.splice(thisBooksList.filters.indexOf(filterValue), 1);
+          }
+          console.log('filters:', thisBooksList.filters);
+        }
+        thisBooksList.filterBooks();
+      });
+    }
+    filterBooks() {
+      const thisBooksList = this;
+
+      for (let book of dataSource.books) {
+        let hidden = false;
+        const selected = document.querySelector('.book__image[data-id="' + book.id + '"]');
+
+        for (let filter of thisBooksList.filters) {
+          if (!book.details[filter]) {
+            hidden = true;
+            break;
+          }
+        }
+        if (hidden == true) {
+          selected.classList.add(classNames.hidden);
+        } else {
+          selected.classList.remove(classNames.hidden);
+        }
+      }
     }
   }
   new BooksList();
